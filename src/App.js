5cbae7e118e9ec6339679477635f3335bc4d59c0
neduardoaguire <neduardoaguirre@gmail.com>
2021-03-29 15:41:13 -0300

    Fetching images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 
 function App() {
   const [search, setSearch] = useState('');
   const [images, setImages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [empty, setEmpty] = useState(true);
+
+  useEffect(() => {
+    const callAPI = async () => {
+      setLoading(true);
+      const IMAGES_PER_PAGE = 36;
+      const API_KEY = '18830736-e1b07399b7ae644ffcb8341db';
+      const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${search}&per_page=${IMAGES_PER_PAGE}&page=${currentPage}`;
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(images);
+      if (data.hits.length === 0) {
+        setEmpty(false);
+        console.log(empty);
+      }
+      setTimeout(() => {
+        scroll();
+        setLoading(false);
+        setImages(data.hits);
+        const totalPages = Math.ceil(data.totalHits / IMAGES_PER_PAGE);
+        setTotalPages(totalPages);
+      }, 1500);
+    };
+    callAPI();
+  }, [search, currentPage]);
+
+  const scroll = () => {
+    const top = document.querySelector('.jumbotron');
+    top.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <div className="container">
--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Error = () => {
-  return <div></div>;
+const Error = ({ message }) => {
+  return (
+    <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 my-3 p-3 text-center alert alert-danger">
+      {message}
+    </div>
+  );
 };
 
 export default Error;
