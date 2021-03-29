adfef0f7450e4b684871da51c4b4ae7a208dae15
neduardoaguire <neduardoaguirre@gmail.com>
2021-03-29 14:38:36 -0300

    Created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div>Pixabay</div>;
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center text-uppercase">Search for images</p>
+      </div>
+      <div className="row justify-content-center"></div>
+      <div className="row justify-content-center mb-3"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ImageListing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageListing = () => {
+  return <div></div>;
+};
+
+export default ImageListing;
